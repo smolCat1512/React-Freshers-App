616ebe5ad058411cb02b47d7e264d1287485886d
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-06 14:16:11 +0100

    IT Services completed pretty much, also added campus guide link to campusmap page

--- src/pages/ITServices.js
+++ src/pages/ITServices.js
@@ -6,6 +6,21 @@ function ITServices() {
         <main>
             <h1>IT Services Page</h1>
 
+            <p>Starting university, like any change of situation in the 20th century, comes with
+                technological changes and adaptions, in the case of Edge Hill this is getting
+                familiar with new applications and systems &#128530;.
+            </p>
+
+            <p>Do not worry about the overload of information, most of the information in this 
+                application, and important guidance you need, will be presented in your welcome week
+                classes &#128513;.
+            </p>
+
+            <p>The system used to deliver teaching is called <strong>Blackboard</strong>, get
+            familiar with it as it will be your friend/enemy/frenemy for the next 3 years! It is
+             part of a suite of tools and apps used under the umbrella of <strong>Learning Edge.</strong>
+            </p>
+
             <p>Whilst the coronavirus pandemic is still in flux most teaching is being delivered
                 online using presentation and participation tools to enable virtual teaching.
             </p>
@@ -13,6 +28,48 @@ function ITServices() {
             <p>Below are guides and video links to help try to overcome any issues you have around
                 this.
             </p>
+
+            <h2>Learning Edge and Blackboard</h2>
+
+            <iframe width="600" height="350" src="https://edgehill.cloud.panopto.eu/Panopto/Pages/Embed.aspx?id=ff170c7a-299b-4f85-a86c-aca700d8ffa4&autoplay=false&offerviewer=true&showtitle=true&showbrand=false&start=0&interactivity=all" 
+            title="Edge Hill Univeristy Campus Tour Video" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen></iframe>
+
+            <h2>Learning Edge and Blackboard Ultra links:</h2>
+
+            <a className="mobile-left-margin" rel="noreferrer" target="_blank" 
+            href="http://eshare.edgehill.ac.uk/1216/1/LTD0003v3.pdf">
+                Learning Edge Basics</a>
+
+                <a className="mobile-left-margin" rel="noreferrer" target="_blank" 
+            href="http://eshare.edgehill.ac.uk/15741/1/LS099_Blackboard_Ultra_Base_Navigation_Overview.pdf">
+                Blackboard Ultra</a>
+
+            <h2>Edge Hill Induction</h2>
+
+            <p>Before starting classes it is advised to read the induction content, this can be found
+                using the below link. This link contains information on learning and support services, 
+                libraries, digital library, uniskills, disability support, ask us and next steps:
+            </p>
+
+            <a className="mobile-left-margin" rel="noreferrer" target="_blank" 
+                href="https://www.edgehill.ac.uk/ls/induction/?tab=induction">
+                Edge Hill Induction Link</a>
+
+            <h2>IT service/support</h2>
+
+            <p>Technology doesn't always work, and sometimes (or most of the time) computers break &#x1F4A5; &#128187;.</p>
+
+            <p>The tech hub/computer science department has a dedicated team of IT technicians, Roger and Michael,
+                how to access, contact and the services provided can be found using the below link to a guide:
+            </p>
+
+            <a className="mobile-left-margin" rel="noreferrer" target="_blank" 
+                href="https://learn-eu-central-1-prod-fleet01-xythos.content.blackboardcdn.com/5b74134a6d3cc/5018094?X-Blackboard-Expiration=1617732000000&X-Blackboard-Signature=OGEHQ3rbLQXl7IeMU2B0c2cEjWC8qgTmsnooNAmvWiA%3D&X-Blackboard-Client-Id=309029&response-cache-control=private%2C%20max-age%3D21600&response-content-disposition=inline%3B%20filename%2A%3DUTF-8%27%27CompSci_Technical_2020.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20210406T120000Z&X-Amz-SignedHeaders=host&X-Amz-Expires=21600&X-Amz-Credential=AKIAZH6WM4PL5M5HI5WH%2F20210406%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=9e32a317aa55c773a763b917bb3aa8f58902ec3795f61d30be26803c988d84a0">
+                IT services link</a>
+
+            <p>Alternatively you can email the team on <strong>computingsupport@edgehill.ac.uk</strong></p>
 
             <h2>Microsoft Teams user guide:</h2>
 
@@ -30,6 +87,8 @@ function ITServices() {
                 <a className="mobile-left-margin" rel="noreferrer" target="_blank" 
             href="https://edgehill.cloud.panopto.eu/Panopto/Pages/Viewer.aspx?id=e4b3b0ab-2da9-475f-8c06-ac4700ca280f">
                 Microsoft Teams video guide link</a>
+
+            <div className="footerspacer"></div>
 
         </main>
         );
