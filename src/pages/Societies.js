fb2bc8da0c93394fd85da90e919bd56d83af380d
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-06 16:11:59 +0100

    json file built for societies page and is iterating in the societies page. Need to get images path working in json as only displaying alt tags currently

--- src/pages/Societies.js
+++ src/pages/Societies.js
@@ -1,10 +1,36 @@
 import React from "react"
 import '../App.css';
+import SocietiesData from '../pages/societies/societies.json';
 
     function Societies() {
         return (
         <main>
             <h1>Societies Page</h1>
+
+            <p>There are many societies at Edge Hill University, the full list can be found at the below
+                link, but also see below for a selection of societies available at Edge Hill.
+            </p>
+
+            <h2>Selected Societies:</h2>
+
+            {SocietiesData.map((societiesDetail, index)=> {
+                return<div className="societiestopmargin">
+                        <img src={societiesDetail.image } width="300" height="300" alt={societiesDetail.title + " logo"} />
+                        <h2>{societiesDetail.title}</h2>
+                        <p>{societiesDetail.description}</p>
+                        <p>Meetings: {societiesDetail.meetings}</p>
+                        <p>Socials: {societiesDetail.social}</p>
+                </div>
+            })}
+
+
+
+
+
+
+
+            <div className="footerspacer"></div>
+
         </main>
         );
     }
