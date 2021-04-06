ac9949d20deba1eb6430fbcf9ce1877b53dd155c
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-06 12:03:11 +0100

    Renamed all images to jpgs

--- src/footer.js
+++ src/footer.js
@@ -8,13 +8,13 @@ function Footer() {
         <div class="footerWrapper">
           <h2>Socials:</h2>
           <a rel="noreferrer" target="_blank" href="https://www.facebook.com/studentsehu">
-            <img class="socials" src="images/facebook.png" alt="Facebook link" />
+            <img class="socials" src="images/facebook.jpg" alt="Facebook link" />
           </a>
           <a rel="noreferrer" target="_blank" href="https://twitter.com/StudentsEHU">
-            <img class="socials" src="images/twitter.png" alt="Twitter link" />
+            <img class="socials" src="images/twitter.jpg" alt="Twitter link" />
           </a>
           <a rel="noreferrer" target="_blank" href="https://www.linkedin.com/school/edge-hill-university">
-            <img class="socials" src="images/linkedin.png" alt="LinkedIn link" />
+            <img class="socials" src="images/linkedin.jpg" alt="LinkedIn link" />
           </a>
         </div>
       </footer>
