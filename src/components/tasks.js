4a50b62f2da3056fd50e31c1c233cc00b1f195cd
ShaunH1512 <24032620@edgehill.ac.uk>
2021-04-08 12:35:49 +0100

    mobile responsive parts built in for reminders/tasks page

--- src/components/tasks.js
+++ src/components/tasks.js
@@ -2,25 +2,7 @@ import React from 'react'
 // import { useState, useEffect } from 'react'
 import Task from './Task'
 
-
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-
-    // const [tasks, setTasks] = useState([])
-
-    // useEffect( () =>{
-    //     const getTasks = async() => {
-    //         const tasksFromServer = await fetchTasks()
-    //         setTasks(tasksFromServer)
-    //     }
-    //     fetchTasks()
-    // }, [])
-
-    // // Fetch Tasks
-    // const fetchTasks = async () => {
-    //     const res = await fetch('http://localhost:5000/tasks')
-    //     const data = await res.json()
-    //     return data
-    // }
     
     return (
         <div>
